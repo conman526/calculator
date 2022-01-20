a30d87e24a56c9d09d703a01904514df4e08b1e4
conman526 <connor.sauby@gmail.com>
2022-01-19 21:40:41 -0800

    Barebones calculator created in CSS.

---
+++ script.js
@@ -0,0 +1,15 @@
+// create functions for add, subctract, divide, multiply, input to percentage
+
+// create function that will take an operator function above with 2 numbers
+
+// create functions that populate display when you press number buttons. Store this value.
+
+// make these numbers populate the operator function and return to the display.
+
+// get clear button to work
+
+// get backspace button to work
+
+// allow user to string multiple operations together.
+
+// round decimals on display
